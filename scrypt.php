48b3930fad11f836ee3b10d3b681cbf594ce3388
scripcrypt <scripcrypt@gmail.com>
2023-04-30 16:18:19 +0900

    fixed comment

--- scrypt.php
+++ scrypt.php
@@ -25,7 +25,7 @@ function array_mix( $array = [] ) {
 }
 
 
-	// array ex .. mix 2 array
+	// array ex .. mix two array
 	function __array_ex( &$array0, &$array2 ) {
 		if ( is_array( $array2 ) ) {
 			foreach ( $array2 as $key2 => &$val2 ) {
